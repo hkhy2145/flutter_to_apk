9adc03d7db140d46daa4d6e98dfbde539bd19e00
hkhy2145 <132103457+hkhy2145@users.noreply.github.com>
2023-11-07 10:57:47 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:telephony/telephony.dart';
+import 'home.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeScreen(),
+      title: 'FlutterViz Demo',
+
+      /// TODO Replace with your first screen class name
+      home: hkhtatt(),
     );
   }
 }
 
-class HomeScreen extends StatefulWidget {
-  HomeScreen({Key? key}) : super(key: key); // أضفت مُعرِّف "key" هنا
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  String latestSms = "No SMS available";
-
-  Future<void> fetchLatestSms() async {
-    Telephony telephony = Telephony.instance;
-    List<SmsMessage> messages = await telephony.getInboxSms();
-    if (messages.isNotEmpty) {
-      SmsMessage latestMessage = messages.first;
-      setState(() {
-        latestSms = "Latest SMS: ${latestMessage.body}";
-      });
-    }
-  }
-
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('SMS Reader'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(latestSms),
-            ElevatedButton(
-              onPressed: fetchLatestSms,
-              child: const Text('Fetch Latest SMS'),
-            ),
-          ],
-        ),
-      ),
+      body: Text("My FlutterViz Demo"),
     );
   }
 }
